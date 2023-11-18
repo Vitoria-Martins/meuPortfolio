26a6322229cc4d084c8bddbe3860ceb31eba02d7
vitoria299 <vitorianogueira299@gmail.com>
2023-11-18 12:27:00 -0300

    new version

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function abrirSobre() {
   window.location.href = "sobre.html";
 }
+
+const imagens = document.querySelectorAll(".imagem");
+const textos = document.querySelectorAll(".textMouse");
+
+imagens.forEach((imagem, index) => {
+  const texto = textos[index];
+  imagem.addEventListener("mouseover", function () {
+    texto.style.opacity = "1"; // texto visível
+  });
+
+  imagem.addEventListener("mouseout", function () {
+    texto.style.opacity = "0"; // texto invisível
+  });
+});
